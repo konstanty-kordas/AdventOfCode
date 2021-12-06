599b3675be9c930795ef444592bf1062c30175d2
konstanty-kordas <konstekkor@gmail.com>
2021-12-06 09:53:37 +0100

    day6

---
+++ day6/day6.py
@@ -0,0 +1,14 @@
+T = []
+with open('day6.txt') as file:
+    T = [int(f) for f in file.read().split(',')]
+mod = [0 for j in range(9)]
+for fish in T:
+    mod[fish%7]+=1
+for day in range(256):
+    current = mod[0]
+    for i in range(1,len(mod)):
+        mod[i-1] = mod[i]
+    mod[6]+=current
+    mod[8]=current
+print(sum(mod))
+
