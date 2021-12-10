c23c1879d884c8654654f870c0d3594efcc02a82
Konstanty Kordas <kostekkor@gmail.com>
2021-12-10 10:30:34 +0100

    day8 fix

--- day8/day8.py
+++ day8/day8.py
@@ -13,58 +13,63 @@ for i in range(len(TO)):
     while sum([len(k) for k in numbers])<45:
         j = 0
         while j<len(TI[i]):
-            if numbers[1] and numbers[4]:
-                helper = [x for x in numbers[4] if x not in numbers[1]]
+            if numbers[1] and numbers[4] and not helper:
+                helper = set([x for x in numbers[4] if x not in numbers[1]])
                 continue
             if len(TI[i][j])==2:
                 numbers[1] = TI[i][j]
-                TI[i].pop(j)
+                TI[i].remove(TI[i][j])
                 continue
             if len(TI[i][j])==3:
                 numbers[7] = TI[i][j]
-                TI[i].pop(j)
+                TI[i].remove(TI[i][j])
                 continue
             if len(TI[i][j])==4:
                 numbers[4] = TI[i][j]
-                TI[i].pop(j)
+                TI[i].remove(TI[i][j])
                 continue
             if len(TI[i][j])==7:
                 numbers[8] = TI[i][j]
-                TI[i].pop(j)
+                TI[i].remove(TI[i][j])
                 continue
-            if len(TI[i][j])== 6:
+            if len(TI[i][j]) == 6:
+                # print("LEN 6")
                 if numbers[4]:
-                    if numbers[4] in TI[i][j]: #WRONG
+                    if numbers[4].issubset(TI[i][j]): #WRONG
                         numbers[9] = TI[i][j]
-                        TI[i].pop(j)
+                        TI[i].remove(TI[i][j])
                         continue
                 if numbers[9] and numbers[1]:
-                    if numbers[1] in TI[i][j]: #WRONG
+                    if numbers[1].issubset(TI[i][j]): #WRONG
                         numbers[0] = TI[i][j]
-                        TI[i].pop(j)
+                        TI[i].remove(TI[i][j])
                         continue
                 if numbers[9] and numbers[0]: 
                     numbers[6] = TI[i][j]
-                    TI[i].pop(j)
+                    TI[i].remove(TI[i][j])
                     continue
-            if len(TI[i][j])== 5:
+            if len(TI[i][j]) == 5:
                 if numbers[1]:
-                    if numbers[1] in TI[i][j]: #WRONG
+                    if numbers[1].issubset(TI[i][j]): #WRONG
                         numbers[3] = TI[i][j]
-                        TI[i].pop(j)
+                        TI[i].remove(TI[i][j])
                         continue
                 if helper:
-                    if helper in TI[i][j]:  #WRONG
+                    if helper.issubset(TI[i][j]):  #WRONG
                         numbers[5] = TI[i][j]
-                        TI[i].pop(j)
+                        TI[i].remove(TI[i][j])
                         continue
                 if numbers[3] and numbers[5]:
                     numbers[2] = TI[i][j]
-                    TI[i].pop(j)
+                    TI[i].remove(TI[i][j])
                     continue
             j+=1
-    print(numbers)
     for j in range(len(TO[i])):
-        outNum+=TO[i][j]
+        for num in range(len(numbers)):
+            if TO[i][j]==numbers[num]:
+                outNum+=str(num)
+    outNum = int(outNum)
+    sumAppears +=outNum
 
-print(appears)
+
+print(sumAppears)
